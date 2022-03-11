5d75d0b0aaa46da7612f6f6060eb0fd73447d4af
Zach Hutz <zachhutz0@gmail.com>
2022-03-11 13:30:31 -0500

    Removed reload and help, combined reload functionality with reset

--- popup.js
+++ popup.js
@@ -1,23 +1,16 @@
 let clicked = false;
-document.getElementById("reload").addEventListener(
-	"click",
-	() => {
-		chrome.runtime.reload();
-	},
-	{ once: true }
-);
-document.getElementById("revert").addEventListener("click", async () => {
+document.getElementById("reset").addEventListener("click", async () => {
 	let [tab] = await chrome.tabs.query({
 		active: true,
 		currentWindow: true,
 	});
 	if (clicked === true) {
-		chrome.scripting.executeScript({
+		await chrome.scripting.executeScript({
 			target: { tabId: tab.id },
 			files: ["revert.js"],
 		});
+		chrome.runtime.reload();
 	}
-	clicked = false;
 });
 document.getElementById("cad").addEventListener("click", async () => {
 	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -25,20 +18,5 @@ document.getElementById("cad").addEventListener("click", async () => {
 	chrome.scripting.executeScript({
 		target: { tabId: tab.id },
 		files: ["content.js"],
-	});
-});
-document.getElementById("help").addEventListener("click", async () => {
-	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-	const help = () => {
-		alert(
-			`CAD: Convert on-page currencies to Canadian dollars
-			\n\nRevert: Convert back to USD
-			\n\nReset: Reloads the extension; each button can only be pressed once, so after using both buttons the extension will need to be reset.
-			\nWARNING: resetting the extension and clicking CAD again after it has already taken effect WILL once again perform the conversion on the CAD amount`
-		);
-	};
-	chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		func: help,
 	});
 });
